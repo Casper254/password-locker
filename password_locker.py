53a620399b0510c5685a672d2048dba452ed6486
Casper254 <wema.mugofwa@gmail.com>
2018-11-02 12:52:51 +0300

    test run saving contacts

--- password_locker.py
+++ password_locker.py
@@ -4,13 +4,17 @@ class Account:
     '''
     account_details = []
 
+#this function puts the info in variables
     def __init__(self,username,password):
                 
         self.username = username
         self.password = password
     
+#This function saves the account information
     def save_account(self):
         '''
         saves account objects into the account_details
         '''
         Account.account_details.append(self)
+    
+#
--- password_locker_test.py
+++ password_locker_test.py
@@ -19,12 +19,32 @@ class TestAccount(unittest.TestCase):
         self.assertEqual(self.new_account.username,'Wema')
         self.assertEqual(self.new_account.password,'lock')
 
+#saves account details
     def test_save_account(self):
         '''
         tests if the account object is saved into the account_details
         '''
         self.new_account.save_account()
         self.assertEqual(len(Account.account_details),1)
+
+#tearDown method
+    def tearDown(self):
+        '''
+        this method cleans up after the test case has run.
+        '''
+        Account.account_details = []
+
+#saves multiple accounts
+    def test_save_multiple_account(self):
+        '''
+        this test checks if we can save multiple accounts from our object
+        '''
+        self.new_account.save_account()
+        test_account = Account('Test','passie')
+        test_account.save_account()
+        self.assertEqual(len(Account.account_details),2)
+
+
 
 if __name__ == '__main__':
     unittest.main()
