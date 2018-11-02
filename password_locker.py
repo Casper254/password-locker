e31cb72cff6ff8fd656dbefdc3821396b06a01fe
Casper254 <wema.mugofwa@gmail.com>
2018-11-02 14:54:38 +0300

    delete account feature

--- password_locker.py
+++ password_locker.py
@@ -2,19 +2,24 @@ class Account:
     '''
     Class that generates new instances of account
     '''
-    account_details = []
+    account_list = []
 
 #this function puts the info in variables
-    def __init__(self,username,password):
+    def __init__(self,username,password, email):
                 
         self.username = username
         self.password = password
+        self.email = email
     
 #This function saves the account information
     def save_account(self):
         '''
         saves account objects into the account_details
         '''
-        Account.account_details.append(self)
-    
-#
+        Account.account_list.append(self)
+
+    def delete_account(self):
+        '''
+        this method deletes a saved account from the account list
+        '''
+        Account.account_list.remove(self)
--- password_locker_test.py
+++ password_locker_test.py
@@ -9,7 +9,7 @@ class TestAccount(unittest.TestCase):
         '''
         Set up method runs before each test case.
         '''
-        self.new_account = Account('Wema','lock')
+        self.new_account = Account('Wema','lock', 'mug@good.com')
 
     def test_init(self):
         '''
@@ -18,6 +18,7 @@ class TestAccount(unittest.TestCase):
 
         self.assertEqual(self.new_account.username,'Wema')
         self.assertEqual(self.new_account.password,'lock')
+        self.asserEqual(self.new_account.email,'mug@good.com')
 
 #saves account details
     def test_save_account(self):
@@ -25,14 +26,14 @@ class TestAccount(unittest.TestCase):
         tests if the account object is saved into the account_details
         '''
         self.new_account.save_account()
-        self.assertEqual(len(Account.account_details),1)
+        self.assertEqual(len(Account.account_list),1)
 
 #tearDown method
     def tearDown(self):
         '''
         this method cleans up after the test case has run.
         '''
-        Account.account_details = []
+        Account.account_list = []
 
 #saves multiple accounts
     def test_save_multiple_account(self):
@@ -40,11 +41,22 @@ class TestAccount(unittest.TestCase):
         this test checks if we can save multiple accounts from our object
         '''
         self.new_account.save_account()
-        test_account = Account('Test','passie')
+        test_account = Account('Test','passie', 'mug@good.com')
         test_account.save_account()
-        self.assertEqual(len(Account.account_details),2)
+        self.assertEqual(len(Account.account_list),2)
 
+#deleting accounts
+    def test_delete_account(self):
+        '''
+        this method test if we can remove a account from the account list
+        '''
+        self.new_account.save_account()
+        test_account = Account('Test', 'passie', 'mug@good.com')
+        test_account.save_account()
 
+        self.new_account.delete_account()
+        self.assertEqual(len(Account.account_list),1)
 
+#finding 
 if __name__ == '__main__':
     unittest.main()
